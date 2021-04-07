b48ed3b6108c14113e7779f5f42140f4d7826c99
Lichuan-Deng <lichuan.deng97@hotmail.com>
2021-04-07 17:48:29 +0800

    Create rnn.py

---
+++ Practice/rnn.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+
+class RNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
+        super(RNN, self).__init__()
+        # [0-10001] => [100]
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        # [100] => [256]
+        self.rnn = nn.LSTM(embedding_dim, hidden_dim, num_layers=2,
+                           bidirectional=True, dropout=0.5)
+        # [256*2] => [1]
+        self.fc = nn.Linear(hidden_dim*2, 1)
+        self.dropout = nn.Dropout(0.5)
+    def forward(self,x):
+        # [seq, b, 1] => [seq, b, 100]
+        embedding = self.dropout(self.embedding(x))
+        # output: [seq, b, hid_dim*2]
+        # hidden/h: [num_layers*2, b, hid_dim]
+        # cell/c: [num_layers*2, b, hid_dim]
+        output, (hidden, cell) = self.rnn(embedding)
+        # [num_layers*2, b, hid_dim] => 2 of [b, hid_dim] => [b, hid_dim*2]
+        hidden = torch.cat([hidden[-2], hidden[-1]], dim=1)
+        # [b, hid_dim*2] => [b, 1]
+        hidden = self.dropout(hidden)
+        out = self.fc(hidden)
+        return out
