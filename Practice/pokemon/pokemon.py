b45f6b85ef098a5910b1548e5e32981e67f5f6e1
Lichuan-Deng <lichuan.deng97@hotmail.com>
2021-04-10 17:55:22 +0800

    Update pokemon.py

--- Practice/pokemon/pokemon.py
+++ Practice/pokemon/pokemon.py
@@ -3,6 +3,8 @@ import os, glob
 import random, csv
 
 from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
 
 
 
@@ -15,7 +17,7 @@ class Pokemon(Dataset):
         self.resize = resize
 
         self.name2label = {} # "squirtle": 0
-        for name in os.listdir(os.path.join(root)):
+        for name in sorted(os.listdir(os.path.join(root))):
             if not os.path.isdir(os.path.join(root, name)):
                 continue
 
@@ -24,42 +26,83 @@ class Pokemon(Dataset):
         print(self.name2label)
 
         # image, label
-        self.load_csv('images.csv')
+        self.images, self.labels = self.load_csv('images.csv')
+
+        if mode == 'train': # 60%
+            self.images = self.images[:int(0.6*len(self.images))]
+            self.labels = self.labels[:int(0.6*len(self.labels))]
+        elif mode == 'val': # 20%
+            self.images = self.images[int(0.6 * len(self.images)):int(0.8 * len(self.images))]
+            self.labels = self.labels[int(0.6 * len(self.labels)):int(0.8 * len(self.labels))]
+        else:
+            self.images = self.images[int(0.8 * len(self.images)):]
+            self.labels = self.labels[int(0.8 * len(self.labels)):]
 
     def load_csv(self, filename):
 
-        images = []
-        for name in self.name2label.keys():
-            # 'pokemon\\bulbasaur\\00000000.png'
-            images += glob.glob(os.path.join(self.root, name, '*.png'))
-            images += glob.glob(os.path.join(self.root, name, '*.jpg'))
-            images += glob.glob(os.path.join(self.root, name, '*.jpeg'))
-        print(len(images), images)
+        if not os.path.exists(os.path.join(self.root, filename)):
 
-        random.shuffle(images)
-        with open(os.path.join(self.root, filename), mode='w', newline='') as f:
-            writer = csv.writer(f)
-            for img in images: # 'pokemon\\bulbasaur\\00000000.png'
-                name = img.split(os.sep)[-2]
-                label = self.name2label[name]
-                # 'pokemon\\bulbasaur\\00000000.png', 0
-                writer.writerow([img, label])
-            print('writen into csv file:', filename)
+            images = []
+            for name in self.name2label.keys():
+                # 'pokemon\\bulbasaur\\00000000.png'
+                images += glob.glob(os.path.join(self.root, name, '*.png'))
+                images += glob.glob(os.path.join(self.root, name, '*.jpg'))
+                images += glob.glob(os.path.join(self.root, name, '*.jpeg'))
+            print(len(images), images)
+
+            random.shuffle(images)
+            with open(os.path.join(self.root, filename), mode='w', newline='') as f:
+                writer = csv.writer(f)
+                for img in images: # 'pokemon\\bulbasaur\\00000000.png'
+                    name = img.split(os.sep)[-2]
+                    label = self.name2label[name]
+                    # 'pokemon\\bulbasaur\\00000000.png', 0
+                    writer.writerow([img, label])
+                print('writen into csv file:', filename)
+
+        images, labels = [],[]
+        with open(os.path.join(self.root, filename)) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                img, label = row
+                label = int(label)
+
+                images.append(img)
+                labels.append(label)
+        assert len(images) == len(labels)
+
+        return images, labels
 
 
     def __len__(self):
-        pass
+        return len(self.images)
 
 
     def __getitem__(self, idx):
-        pass
+        # idx [0, len(images)]
+        img, label = self.images[idx], self.labels[idx]
+
+        tf = transforms.Compose([
+            lambda x: Image.open(x).convert('RGB'), # string path => image data
+            transforms.Resize((self.resize, self.resize)),
+            transforms.ToTensor(),
+        ])
+
+        img = tf(img)
+        label = torch.tensor(label)
+
+        return img, label
+
+
+
 
 
 def main():
 
+
     db = Pokemon('D:\Projects\pokeman', 224, 'train')
-
-
+    x,y = next(iter(db))
+    print('sample:', x.shape, y.shape, y)
 
 if __name__ == '__main__':
     main()
